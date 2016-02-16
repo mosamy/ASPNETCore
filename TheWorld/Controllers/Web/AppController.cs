57e2f76a06bda23657eafda4d0eecfdef84187f5
mohamed samy <mohamed@bunsendevdergah.bunsendevdergah>
2016-02-15 22:36:25 -0600

    addmvc

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -1,12 +1,17 @@
 using System;
 using Microsoft.AspNet.Mvc;
 
+
 namespace TheWorld.Controllers.Web
 {
 	public class AppController : Controller
 	{
 		public IActionResult Index()
 		{
+			Console.WriteLine("Index Controller") ;
+			Console.WriteLine(View().ViewName.Length) ;
+			Console.WriteLine("Hello") ;
+			
 			return View();
 		}
 	}
--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -15,8 +15,8 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvcCore();
-            //services.AddMvc();
+           // services.AddMvcCore();
+           services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
